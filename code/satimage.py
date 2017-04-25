e1e6133dbf7b8f4795081b6cbf275030bb7b046d
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-04-25 05:55:43 +0530

    Remove redundant file and fix minor bugs.

--- code/satimage.py
+++ code/satimage.py
@@ -2,32 +2,15 @@ import os
 import keras.backend as K
 import numpy as np
 import pandas as pd
-import math
-import matplotlib.cm as cm
-import numpy.ma as ma
 import pylab as pl
+import matplotlib.cm as cm
+import math
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy import misc
-from keras.models import model_from_json
+
+import util
 
 BATCH_SIZE = 20
-
-def restore_model(json_filepath, weights_filepath):
-    """
-    Restore the pretrained model.
-    :param json_filepath: Path of the .json file containing model architecture
-    :param weights_filepath: Path of the .h5 file containing weights of pretrained model
-    :return `model` object
-    """
-    print("Loading model from disk...")
-    json_file = open(json_filepath, "r")
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    model.load_weights(weights_filepath)
-    print("Done")
-    return model
 
 
 def img_data_generator(file_paths, batch_size):
@@ -87,48 +70,8 @@ def generate_predictions(model, img_dir, out_filepath, batch_size=BATCH_SIZE):
     
     compare = compare[order]
     compare.to_csv(out_filepath, index=False)
-    print("Done")
+    print("Done")  
 
-    
-# -----------------------------------------------------------------------------
-# https://github.com/julienr/
-# ipynb_playground/blob/master/keras/convmnist/keras_cnn_mnist.ipynb
-def nice_imshow(ax, data, vmin=None, vmax=None, cmap=None):
-    """Wrapper around pl.imshow"""
-    if cmap is None:
-        cmap = cm.jet
-    if vmin is None:
-        vmin = data.min()
-    if vmax is None:
-        vmax = data.max()
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    im = ax.imshow(data, vmin=vmin, vmax=vmax, interpolation='nearest', cmap=cmap)
-    pl.colorbar(im, cax=cax)
-
-
-def make_mosaic(imgs, nrows, ncols, border=1):
-    """
-    Given a set of images with all the same shape, makes a
-    mosaic with nrows and ncols
-    """
-    nimgs = imgs.shape[0]
-    imshape = imgs.shape[1:]
-
-    mosaic = ma.masked_all((nrows * imshape[0] + (nrows - 1) * border,
-                            ncols * imshape[1] + (ncols - 1) * border),
-                           dtype=np.float32)
-
-    paddedh = imshape[0] + border
-    paddedw = imshape[1] + border
-    for i in xrange(nimgs):
-        row = int(np.floor(i / ncols))
-        col = i % ncols
-
-        mosaic[row * paddedh:row * paddedh + imshape[0],
-        col * paddedw:col * paddedw + imshape[1]] = imgs[i]
-    return mosaic
-    
     
 def show_filter_responses(model, layer_index, input_img_path, save_dir=None, filter_index=None, dpi=100.0, save_original=False):
     """
@@ -156,13 +99,13 @@ def show_filter_responses(model, layer_index, input_img_path, save_dir=None, fil
         display_image = input_img.transpose((1, 2, 0))
     else:
         display_image = np.copy(input_img)
-    display_image = deprocess_image(display_image, alter_dim=False)
+    display_image = util.deprocess_image(display_image, alter_dim=False)
 
     pl.figure(
         figsize=(display_image.shape[0] / dpi, display_image.shape[1] / dpi),
         dpi=dpi
     )
-    nice_imshow(pl.gca(), display_image, cmap=cm.binary)
+    util.nice_imshow(pl.gca(), display_image, cmap=cm.binary)
 
     images = np.array([input_img])
     c1 = layer_f(images)
@@ -175,7 +118,7 @@ def show_filter_responses(model, layer_index, input_img_path, save_dir=None, fil
 
     if filter_index is None:
         grid_dim = int(math.ceil(math.sqrt(c1.shape[0])))
-        out_img = make_mosaic(c1, grid_dim, grid_dim)
+        out_img = util.make_mosaic(c1, grid_dim, grid_dim)
     else:
         out_img = c1[filter_index]
     
@@ -185,40 +128,12 @@ def show_filter_responses(model, layer_index, input_img_path, save_dir=None, fil
             prefix += "_filter_" + str(filter_index)
         if save_original:
             misc.imsave(os.path.join(save_dir, prefix + "_input.png"), display_image)
-        misc.imsave(os.path.join(save_dir, prefix + "_output.png"), deprocess_image(out_img, alter_dim=False))
+        misc.imsave(os.path.join(save_dir, prefix + "_output.png"), util.deprocess_image(out_img, alter_dim=False))
 
     pl.figure(
         figsize=(out_img.shape[0] / dpi, out_img.shape[1] / dpi),
         dpi=dpi
     )
     pl.suptitle(layer.name)
-    nice_imshow(pl.gca(), out_img, cmap=cm.binary)
+    util.nice_imshow(pl.gca(), out_img, cmap=cm.binary)
 
-    
-# -----------------------------------------------------------------------------
-
-# https://blog.keras.io/
-# how-convolutional-neural-networks-see-the-world.html
-
-# http://ankivil.com/visualizing-deep-neural-networks-classes-and-features/
-
-# -----------------------------------------------------------------------------
-def deprocess_image(x, alter_dim=True):
-    """
-    Utility function to convert a tensor into a valid image
-    """
-    # normalize tensor: center on 0., ensure std is 0.1
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-    if alter_dim and K.image_dim_ordering() == 'th':
-        x = x.transpose((1, 2, 0))
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
